8905bc697a789a9d96eb07e720196d7cd6ff4b2d
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-03 10:57:22 +0300

    completed assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  const matchPath = useRouteMatch();
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={matchPath.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${matchPath.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
